d94863f6a9df95fc2a3283f92fa67c520eb23cd0
Isaac Chamorro <ischamorropu@uide.edu.ec>
2024-06-12 13:46:27 -0500

    Add files via upload
    Avannce del Codigo del Ahorcado con las condiciones de la unidad 3

---
+++ El Ahorcado.py
@@ -0,0 +1,39 @@
+def mostrar_menu_principal():
+    print("Bienvenido al Juego del Ahorcado")
+    print("Selecciona el modo de juego:")
+    print("1. Adivinar Palabras")
+    print("2. Modo Versus")
+    opcion = input("Ingresa el número de tu elección: ")
+    return opcion
+
+def seleccionar_nivel_dificultad():
+    print("Selecciona el nivel de dificultad:")
+    print("1. Fácil")
+    print("2. Medio")
+    print("3. Difícil")
+    nivel = input("Ingresa el número de tu elección: ")
+    return nivel
+
+def solicitar_nombre_jugador():
+    nombre = input("Ingresa tu nombre: ")
+    return nombre
+
+def main():
+    nombre_jugador = solicitar_nombre_jugador()
+    print(f"Hola, {nombre_jugador}!")
+    opcion = mostrar_menu_principal()
+
+    if opcion == "1":
+        nivel = seleccionar_nivel_dificultad()
+        print(f"{nombre_jugador}, has seleccionado el nivel: {nivel}")
+       
+    elif opcion == "2":
+        print("Modo Versus seleccionado")
+        # Espacio para desarrollar modo versus 
+    else:
+        print("Opción no válida")
+
+if __name__ == "__main__":
+    main()
+
+
